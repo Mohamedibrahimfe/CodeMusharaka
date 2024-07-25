de2d337f97cee7c8931f3bab6c324da3b1acd551
Mohamedibrahimfe <147580141+Mohamedibrahimfe@users.noreply.github.com>
2024-07-25 18:05:49 +0300

    try to catch api (failed)

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// var requestOptions = {
+//     method: 'GET',
+//     redirect: 'follow'
+// };
+
+//     fetch("https://tarmeezacademy.com/api/v1/posts?limit=5", requestOptions)
+//         .then(response => response.text())
+//         .then(result => console.log(result))
+//         .catch(error => console.log('error', error));
