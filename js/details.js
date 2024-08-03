20659fb138a3da05e73fc6671b4f4a7cc410c4dc
Mohamedibrahimfe <147580141+Mohamedibrahimfe@users.noreply.github.com>
2024-08-03 07:28:58 +0300

    alerts solved in postDetails

--- js/details.js
+++ js/details.js
@@ -1,19 +1,8 @@
 const urlParams = new URLSearchParams(window.location.search);
 const passUrlParams = urlParams.get("id");
 fillNewPage(passUrlParams);
-checkIfUserIsLoggedIn();
-function checkIfUserIsLoggedIn() {
-  if (
-    localStorage.getItem("token") == null ||
-    localStorage.getItem("token") == undefined ||
-    localStorage.getItem("token") == ""
-  ) {
-    return;
-  } else {
-    console.log(document.getElementById("addComment"));
-    // document.getElementById("comment")
-  }
-}
+
+
 function fillNewPage(id) {
   axios({
     method: "get",
@@ -29,20 +18,19 @@ function fillNewPage(id) {
       const aouthor = post.author.name;
       const title = `<h1 class="py-5">${aouthor} Post</h1>`;
       const comments = post.comments.map((comment) => {
-        time = comment.author.created_at.slice(0, 10);
-        time += " " + comment.author.created_at.slice(11, 19);
+        let timeF = comment.author.created_at.slice(0, 10);
+        let timeS = " " + comment.author.created_at.slice(11, 19);
         return `
         <div class="d-flex">
             <div class="d-flex flex-column w-100">
-                <h4>
+                <h4 class="py-2 pt-0 mt-0">
                     <img class="rounded-circle border border-2" src="${comment.author.profile_image}" width="40px" height="40px" alt="image">
                     <span class="card-text h6 ><small class="text-muted">@${comment.author.username}</small></span>
                 </h4>
-                <br>
                 <div class="d-flex justify-content-between h6 font-weight-light font-italic ">
-                    <h5 class="card-text d-inline">${comment.body}</h5> <span class="text-muted h6 font-weight-light font-italic font-size-sm">Date: ${time}</span>
+                    <h5 class="card-text d-inline m-0 py-0">${comment.body}</h5> <span id="time" class="text-muted h6 font-weight-light font-italic font-size-sm">Date: ${timeF}<span class="d-block"">Time: ${timeS}</span>
                 </div>               
-                <hr>
+                <hr class="w-100 mx-auto my-0 py-0">
             </div>
         </div>
         `;
@@ -59,7 +47,7 @@ function fillNewPage(id) {
                         </div>
                         <img class="p-2 w-100" src="${post.image}"
                             alt="Post image">
-                        <span id="time" class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger time">${post.created_at}
+                        <span  class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger time">${post.created_at}
                         </span>
                         <div class="card-body">
                             <h5 class="card-title">${post.title}</h5>
@@ -67,23 +55,21 @@ function fillNewPage(id) {
                                 ${post.body}
                             </p>
                         </div>
-                        <div class="card-header py-3 pb-0 mb-0 px-2">
+                        <div class="card-header py-3 mb-0 px-2 d-flex">
                             <button type="button" class="btn card-title pb-0 mb-0">
                                 <i class="fa-regular fa-comment "></i>
                                 <span>(${post.comments_count})</span>
                                 Comments
                                 <span id="postTags" class=" rounded-5 text-center px-2 text-light mx-2"> </span>
                             </button>                      
-                            </p>
+                            <i class="fa-regular fa-edit ms-auto p-2 font-bold"></i>
                         </div>
-                        <div class="card-footer p-3 " id="commentsPlace">
+                        <div class="card-footer" id="commentsPlace">
                             ${comments}
                         </div>
-                        <div class="p-3 ">
-                            <button onclick="addComment(${post.id})" class="btn btn-primary p-2" id="addComment">Add a comment </button>
-                        </div>
-                        <div class="card-footer p-3 ">
-                            <input type="text" class="form-control " id="comment" placeholder="Add a comment">
+                        <div id="commentSection" class="bg-light p-3  d-flex">
+                        <input type="text" class="form-control d-inline" id="comment" placeholder="Add a comment">
+                        <button onclick="addComment(${post.id})" class="btn btn-primary p-2 d-inline"  id="addComment">Send comment </button>
                         </div>    
                     </div>
                 </div>
@@ -109,46 +95,32 @@ function addComment(id) {
     },
   })
     .then((response) => {
-      console.log(response.data);
-      getAllComments(id);
+      fillNewPage(id);
     })
-    .catch((error) => console.log(error.message));
+    .catch((error) => {
+      showSuccessMessage(error.response.data.message, "danger");
+    });
+}
+// checkIfUserIsLoggedIn();
+function checkIfUserIsLoggedIn() {
+  if (localStorage.getItem("token") === null) {
+    return;
+  } else {
+    console.log(document.getElementById("commentSection"));
+    document.getElementById("commentSection").style.border = "red";
+  }
 }
 
-function getAllComments(id) {
-  const token = localStorage.getItem("token");
+function checkIfUserOwnsPost() {}
+function updatePost(id) {
+  const form = new FormData();
+
   axios({
-    method: "get",
-    url: `${baseUrl}/posts/${id}/comments`,
+    method: "put",
+    url: `${baseUrl}/posts/${id}`,
     headers: {
       Accept: "application/json",
-      Authorization: `Bearer ${token}`,
     },
-  })
-    .then((response) => {
-      const comments = response.data.data.map((comment) => {
-        let date = comment.created_at.slice(0, 10);
-        date += " " + comment.created_at.slice(11, 19);
-        console.log(comment);
-        const content = `
-            <div class="d-flex">
-                <div class="d-flex flex-column w-100">
-                    <h4>
-                        <img class="rounded-circle border border-2" src="${comment}" width="40px" height="40px" alt="image">
-                        <span class="card-text h6 ><small class="text-muted">@${comment}</small></span>
-                    </h4>
-                    <br>
-                    <div class="d-flex justify-content-between h6 font-weight-light font-italic ">
-                        <h5 class="card-text d-inline">${comment.body}</h5> <span class="text-muted h6 font-weight-light font-italic font-size-sm">Date: ${comment.created_at}</span>
-                    </div>               
-                    <hr>
-                </div>
-            </div>
-            `;
-        document.getElementById("time").innerHTML = date;
-        document.getElementById("commentsPlace").innerHTML = content;
-      });
-    })
-    .catch((error) => console.log(error.message));
+    data: form,
+  });
 }
-getAllComments(25612);
